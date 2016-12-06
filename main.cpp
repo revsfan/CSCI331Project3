74a5c2af32fc318570b5216dacbee04027bc0f02
Steven K Scholz <stevenkurtscholz@gmail.com>
2016-12-06 01:52:36 -0600

    Added working string support

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ struct record{
   node *left;
   node *right;
   int key;
+  string stringKey;
 
 };
 
@@ -40,17 +41,24 @@ public:
 
   //Puts a record in a node
   void nodeInsert(record value);
+  void stringNodeInsert(record value);
 
   //Split nodes when there is no more room
   record splitNode(record value);
+  record stringSplitNode(record value);
 
   //split parents nodes
   record splitParent(record value);
+  record stringSplitParent(record value);
 
   node *nextindex(int value);
+  node *stringNextindex(string value);
 
   void display();
+  void stringDisplay();
+
   void displayval();
+  void stringDisplayval();
 
 }; //END NODE CLASS
 
@@ -134,6 +142,7 @@ class bPTree{
   node *root;
 
   int keys[100];//can change this to whatever
+  string stringKeys[100];
   int totalNumberOfKeys;
 
     public:
@@ -147,21 +156,25 @@ class bPTree{
 
           //Inserts value into tree
           void insert(int value);
+          void stringInsert(string value);
 
 
           //bool checkindex(int value,int choice);
 
           //display tree
           void showTree();
+          void stringShowTree();
 
           //goes through the tree to display all of the values
           void listValues();
+          void stringListValues();
 
 
 
 
           // deletes a value from the tree
           void Delete(int value);
+          void stringDelete(string value);
 };
 
 void node::display()
@@ -174,21 +187,48 @@ void node::display()
 
   cout<<"]";
 }
+
+void node::stringDisplay()
+{
+
+  cout << "[";
+
+  for(int i = 0; i < numberOfKeys; i++)
+    cout << data[i].stringKey <<" ";
+
+  cout << "]";
+}
+
+
+
+
 void node::displayval()
 {
 
   for(int i = 0;i < numberOfKeys; i++)
-  cout << data[i].key << endl;
+    cout << data[i].key << endl;
 }
 
-node *node::nextindex(int value)
+
+void node::stringDisplayval()
 {
 
-  if(this->data[0].left==NULL && this->data[0].right==NULL)
-  return(this);
+  for(int i = 0;i < numberOfKeys; i++)
+    cout << data[i].stringKey << endl;
+}
+
+
+
+node *node::nextindex(int value){
+
+    int i;
+
+  if(this-> data[0].left == NULL && this->data[0].right == NULL)
+        return(this);
+
   else{
-    for(int i = 0; i < numberOfKeys; i++)
-    {
+    for(i = 0; i < numberOfKeys; i++){
+
       if(value < data[i].key)
         return data[i].left;
       else if(value == data[i].key)
@@ -202,10 +242,52 @@ node *node::nextindex(int value)
 
 }
 
+
+
+
+
+
+
+node *node::stringNextindex(string value){
+
+    int i;
+
+  if(this-> data[0].left == NULL && this->data[0].right == NULL)
+        return(this);
+
+  else{
+    for(i = 0; i < numberOfKeys; i++){
+
+      if(value < data[i].stringKey)
+        return data[i].left;
+      else if(value == data[i].stringKey)
+        return data[i].right;
+      else if(value > data[i].stringKey && value < data[i+1].stringKey)
+        return data[i].right;
+    }
+    return data[i - 1].right;
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void node::nodeInsert(record value)
 {
-
-    for(int i = numberOfKeys-1; i >= 0 && data[i].key > value.key; i--)
+    int i = numberOfKeys - 1;
+    for(i ; i >= 0 && data[i].key > value.key; i--)
     data[i + 1] = data[i];
 
     data[i + 1] = value;
@@ -213,6 +295,36 @@ void node::nodeInsert(record value)
     data[i + 1].right = value.right;
     numberOfKeys++;
 }
+
+
+
+
+
+
+void node::stringNodeInsert(record value)
+{
+    int i = numberOfKeys - 1;
+    for(i ; i >= 0 && data[i].stringKey > value.stringKey; i--)
+    data[i + 1] = data[i];
+
+    data[i + 1] = value;
+    data[i + 1].left = value.left;
+    data[i + 1].right = value.right;
+    numberOfKeys++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 record node::splitNode(record value)
 {
@@ -260,6 +372,73 @@ record node::splitNode(record value)
   }
   return(newRecord);
 }
+
+
+
+
+
+
+record node::stringSplitNode(record value)
+{
+  node *newblock;
+  record newRecord;
+
+  int i,
+      j,
+      center;
+
+  center = (numberOfKeys) / 2;
+  newblock = new node;
+
+  //divide the leaf into two parts
+  if(value.stringKey > data[center].stringKey){
+
+
+    for(i = center + 1, j = 0; i <= numberOfKeys; i++, j++)
+      newblock-> data[j] = data[i];
+
+    newblock-> numberOfKeys = numberOfKeys - center - 1;
+    numberOfKeys = numberOfKeys - newblock-> numberOfKeys;
+    newblock-> stringNodeInsert(value);
+    newblock-> first = newblock;
+    newblock-> parent = parent;
+    newRecord.stringKey = newblock-> data[0].stringKey;
+    newRecord.right = newblock;
+    newRecord.left = this;
+    this-> next = newblock;
+  }
+  else{
+
+    for(i = center, j = 0; i < numberOfKeys; i++, j++)
+      newblock-> data[j] = data[i];
+
+    newblock-> numberOfKeys = numberOfKeys - center;
+    numberOfKeys = numberOfKeys - newblock-> numberOfKeys;
+    stringNodeInsert(value);
+    newblock-> parent = parent;
+    newRecord.stringKey = newblock-> data[0].stringKey;
+    newRecord.right = newblock;
+    newRecord.left = this;
+    this-> next = newblock;
+
+  }
+  return(newRecord);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 record node::splitParent(record value){
 
@@ -339,6 +518,97 @@ record node::splitParent(record value){
 
 
 
+
+
+
+record node::stringSplitParent(record value){
+
+  node *newblock;
+  record newRecord;
+
+  int i,
+      j,
+      center;
+
+  center = (numberOfKeys - 2) / 2;
+  newblock = new node;
+
+  //divide the parent into two parts
+  if(value.stringKey > data[center].stringKey){
+
+    for( i = center + 1, j = 0; i <= numberOfKeys; i++, j++)
+      newblock-> data[j] = data[i];
+
+    newblock-> numberOfKeys = numberOfKeys - center - 1;
+    numberOfKeys = numberOfKeys - newblock-> numberOfKeys;
+    newblock-> stringNodeInsert(value);
+    newblock-> first = newblock;
+    newblock-> parent = parent;
+    this-> data[this-> numberOfKeys].right = newblock-> data[0].left;
+    newblock-> data[1].left = newblock-> data[0].right;
+    newRecord.stringKey = newblock-> data[0].stringKey;
+    newRecord.right = newblock;
+    newRecord.left = this;
+    this-> isLeaf = 0;
+    newblock-> isLeaf = 0;
+
+
+    //deleting the first key
+    for( i = 1; i < newblock-> numberOfKeys; i++)
+      newblock-> data[i - 1] = newblock-> data[i];
+
+    newblock-> numberOfKeys--;
+
+    //applying parent
+    for( i = 0; i < newblock-> numberOfKeys; i++){
+
+      newblock-> data[i].right-> parent = newblock;
+    }
+  }else{
+
+    for( i = center, j = 0; i < numberOfKeys; i++, j++)
+      newblock-> data[j] = data[i];
+
+    newblock-> numberOfKeys = numberOfKeys - center;
+    numberOfKeys = numberOfKeys - newblock-> numberOfKeys;
+    stringNodeInsert(value);
+    newblock-> parent = parent;
+    this-> data[this-> numberOfKeys].right = newblock-> data[0].left;
+    newblock-> data[1].left = newblock-> data[0].right;
+    newRecord.stringKey = newblock-> data[0].stringKey;
+    newRecord.right = newblock;
+    newRecord.left = this;
+
+
+    //delete first key from node
+    for( i = 1; i < newblock-> numberOfKeys; i++)
+      newblock-> data[i - 1] = newblock-> data[i];
+
+    newblock-> numberOfKeys--;
+
+
+    //applying the parent
+    for( i = 0; i < newblock-> numberOfKeys; i++){
+
+      newblock-> data[i].right-> parent = newblock;
+    }
+  }//END ELSE
+
+  return(newRecord);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 void bPTree::showTree()
 {
   Queue queue1,
@@ -363,7 +633,7 @@ void bPTree::showTree()
         queue2.enque(currentNode-> data[0].left);
 
         for(int i = 0; i <currentNode-> numberOfKeys; i++)
-            queue2.enque(currentNode->data[i].right);
+            queue2.enque(currentNode-> data[i].right);
 
       }
     }
@@ -371,6 +641,52 @@ void bPTree::showTree()
     queue1 = queue2;
   }
 }
+
+
+
+
+
+
+
+
+void bPTree::stringShowTree()
+{
+  Queue queue1,
+        queue2;
+
+
+  node *currentNode;
+  queue1.enque(root);
+
+  while( !queue1.empty() ){
+
+     queue2.makeEmpty();
+     cout << endl;
+     while( !queue1.empty() ){
+
+      currentNode = queue1.deque();
+      currentNode-> stringDisplay();
+      cout << "  ";
+
+      if( !currentNode-> isLeaf ){
+
+        queue2.enque(currentNode-> data[0].left);
+
+        for(int i = 0; i <currentNode-> numberOfKeys; i++)
+            queue2.enque(currentNode-> data[i].right);
+
+      }
+    }
+
+    queue1 = queue2;
+  }
+}
+
+
+
+
+
+
 
 
 void bPTree::listValues()
@@ -412,16 +728,69 @@ void bPTree::listValues()
 }
 
 
+
+
+void bPTree::stringListValues()
+{
+  Queue q1,
+        q2;
+
+  node *currentNode;
+
+  q1.enque(root);
+
+  while(!q1.empty()){
+
+    q2.makeEmpty();
+
+    cout << "\n";
+
+    while(!q1.empty()){
+
+      currentNode = q1.deque();
+
+      if(currentNode-> isLeaf){
+
+        currentNode-> stringDisplayval();
+      }
+      if( !currentNode-> isLeaf){
+
+
+        q2.enque(currentNode-> data[0].left);
+
+          for(int i = 0;i < currentNode-> numberOfKeys;i++)
+            q2.enque(currentNode-> data[i].right);
+
+      }
+    }
+
+    q1 = q2;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 //insert value into the tree
 void bPTree::insert(int value) // only need value
 {
+
+
   record newRecord;
 
   node *currentNode,
        *temp;
 
-
   newRecord.key = value;
+
 
   newRecord.left = NULL;
   newRecord.right = NULL;
@@ -503,6 +872,115 @@ void bPTree::insert(int value) // only need value
 
   }
 }
+
+
+
+
+
+
+
+
+//insert value into the tree
+void bPTree::stringInsert(string value) // only need value
+{
+
+
+  record newRecord;
+
+  node *currentNode,
+       *temp;
+
+  newRecord.stringKey = value;
+
+
+  newRecord.left = NULL;
+  newRecord.right = NULL;
+  if(root == NULL){
+
+    root = new node;
+    root-> stringNodeInsert(newRecord); //stringNodeInsert
+
+  }else{
+
+    currentNode = root;
+    while( !(currentNode-> isLeaf) )
+      currentNode = currentNode-> stringNextindex(value);
+
+    if(currentNode-> numberOfKeys < mkeys)
+      currentNode-> stringNodeInsert(newRecord);
+
+    else{
+
+      if(currentNode-> isLeaf){
+
+        newRecord = currentNode-> stringSplitNode(newRecord);
+        currentNode-> isLeaf = 1;
+
+      }else{
+
+        newRecord = currentNode-> stringSplitParent(newRecord);
+        currentNode-> isLeaf = 0;
+      }
+
+      while(1){
+
+        if(currentNode == root){
+
+          temp = new node;
+          temp-> data[0] = newRecord;
+          temp-> first = root;
+          temp-> parent = NULL;
+          temp-> data[0].left = newRecord.left;
+          temp-> data[0].right = newRecord.right;
+          temp-> numberOfKeys = 1;
+          root = temp;
+          root-> isLeaf = 0;
+          temp-> first-> parent = temp;
+          temp-> data[0].right-> parent = temp;
+          temp-> data[0].left-> parent = temp;
+          return;
+
+        }else{
+
+          currentNode = currentNode-> parent;
+
+          if(currentNode-> data[0].left != NULL)
+            currentNode-> isLeaf = 0;
+
+          if(currentNode-> numberOfKeys < mkeys){
+
+            currentNode-> stringNodeInsert(newRecord);
+            return;
+
+          }else{
+
+            if(currentNode-> isLeaf){
+
+              newRecord = currentNode-> stringSplitNode(newRecord);
+              currentNode-> isLeaf = 1;
+
+            }else{
+
+              newRecord = currentNode-> stringSplitParent(newRecord);
+              currentNode-> isLeaf = 0;
+            }
+
+          }
+        }
+
+      }
+    }
+
+  }
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -729,12 +1207,242 @@ void bPTree::Delete(int value){
 
 
 
+
+void bPTree::stringDelete(string value){
+
+  node *left,
+       *right;
+
+  record *center;
+
+  node *p,
+       *q;
+
+
+  int i,
+      j,
+      middleIndex;
+
+
+  for(i = 0; p-> data[i].stringKey != value; i++)
+  ;
+
+
+  if(p-> isLeaf){
+
+    for(i = i + 1; i < p-> numberOfKeys; i++)
+      p-> data[i - 1] = p-> data[i];
+    p-> numberOfKeys--;
+
+
+    //delete the value from keys[]
+    for(i = 0; i < totalNumberOfKeys; i++)
+      if(stringKeys[i] == value){
+        for(j = i; j < totalNumberOfKeys; j++)
+        stringKeys[j] = stringKeys[j + 1];
+    }
+    totalNumberOfKeys--;
+  }
+
+
+  while(p-> isLeaf){
+
+    if(p-> numberOfKeys >= mkeys / 2 )
+      return;
+
+    if( p == root ){
+
+      if(p-> numberOfKeys > 0)
+        return;
+      else{
+
+        root = p-> data[0].left;
+        return;
+      }
+    }
+
+
+    q = p-> parent;
+
+    if(q-> data[0].left == p || q-> data[0].right == p){
+
+      left = q-> data[0].left;
+      right = q-> data[0].right;
+      center =& (q-> data[0]);
+      middleIndex = 0;
+
+    }else{
+
+
+      for( i = 0; i < q-> numberOfKeys; i++)
+        if(q-> data[i].left == p || q-> data[i].right == p)
+          break;
+
+      left = q-> data[i].left;
+      right = q-> data[i].right;
+      center =& (q-> data[i]);
+      middleIndex = i;
+    }
+
+    if(right-> numberOfKeys > mkeys / 2){
+
+      left-> data[left-> numberOfKeys].stringKey = center-> stringKey;
+      left-> numberOfKeys++;
+      center-> stringKey = right-> data[0].stringKey;
+
+      for(i = 1; i < right-> numberOfKeys; i++)
+        right-> data[i - 1] = right-> data[i];
+
+      right-> numberOfKeys--;
+      return;
+    }
+
+    //merge left and right
+    else{
+
+      left-> data[left-> numberOfKeys].stringKey = center-> stringKey;
+      left -> numberOfKeys++;
+
+      for(j = 0; j < right-> numberOfKeys; j++)
+        left-> data[left-> numberOfKeys + j] = right-> data[j];
+
+      left-> numberOfKeys += right-> numberOfKeys;
+
+      //delete record from parent
+      for(i = middleIndex + 1; i < q-> numberOfKeys; i++)
+        q-> data[i - 1] = q-> data[i];
+
+      q-> numberOfKeys--;
+      p = q;
+    }
+
+
+  }
+
+  q = recordPosition; //record position is a node
+
+  while( !q-> isLeaf){
+
+    for( i = 0; i < q-> numberOfKeys; i++){
+
+      if(q-> data[i].stringKey == value){
+
+        for( i = i + 1; i < q-> numberOfKeys; i++)
+          q-> data[i - 1] = q-> data[i];
+        q-> numberOfKeys--;
+        break;
+      }
+
+    }
+    if(i == q-> numberOfKeys)
+    break;
+  }
+
+  p = q;
+
+  while( !p-> isLeaf){
+
+    if(p-> numberOfKeys >= mkeys / 2 )
+      return;
+
+    if(p == root ){
+
+      if(p-> numberOfKeys > 0)
+        return;
+      else{
+
+        root = p-> first;
+        return;
+      }
+    }
+
+    q = p-> parent;
+    if(q-> data[0].left == p || q-> data[0].right == p){
+
+      left = q-> data[0].left;
+      right = q-> data[0].right;
+      center =& (q-> data[0]);
+      middleIndex = 0;
+
+    }else{
+
+      for( i = 1; i < q-> numberOfKeys; i++)
+        if(q-> data[i].right == p)
+          break;
+
+      left = q-> data[i - 1].left;
+      right = q-> data[i].right;
+      center =& (q-> data[i]);
+      middleIndex = i;
+    }
+
+
+    //left has an extra key
+    if(left-> numberOfKeys > mkeys / 2){
+
+      for(i = right-> numberOfKeys - 1; i >= 0; i--)
+        right-> data[i + 1] = right-> data[i];
+
+      right-> numberOfKeys++;
+      right-> data[0].stringKey = center-> stringKey;
+      center-> stringKey = left-> data[left-> numberOfKeys - 1].stringKey;
+      left-> numberOfKeys--;
+      return;
+    }
+
+    //right has an extra key
+    else
+
+    if(right-> numberOfKeys > mkeys / 2){
+
+      left-> data[left-> numberOfKeys].stringKey = center-> stringKey;
+      left-> numberOfKeys++;
+      center-> stringKey = right-> data[0].stringKey;
+
+      for(i = 1; i < right-> numberOfKeys; i++)
+        right-> data[i - 1] = right-> data[i];
+
+      right-> numberOfKeys--;
+      return;
+    }
+
+    //merge left and right
+    else{
+
+      left-> data[left-> numberOfKeys].stringKey = center-> stringKey;
+      left-> numberOfKeys++;
+
+      for(j = 0; j < right-> numberOfKeys; j++)
+        left-> data[left-> numberOfKeys + j] = right-> data[j];
+
+      left-> numberOfKeys += right-> numberOfKeys;
+
+      //delete the record from the parent
+      for(i = middleIndex + 1; i < q-> numberOfKeys ; i++)
+      q-> data[i - 1] = q-> data[i];
+      q-> numberOfKeys--;
+      p = q;
+    }
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
 int main(int argc, char **argv){
 
-    //freopen("out.txt","w",stdout); //steals cout stream and writes it to file. This will cause the console to not display anything but will function correctly
+    freopen("out.txt","w",stdout); //steals cout stream and writes it to file. This will cause the console to not display anything but will function correctly
 
-  int number, //size of block
-      value; // key value
+  int number; //size of block
+    string value;// key value
 
 
 
@@ -759,15 +1467,15 @@ int main(int argc, char **argv){
 
 
       while(inputFile >> value){
-        bPlusTree.insert(value); // only need value
+        bPlusTree.stringInsert(value); // only need value
       }
 
 
-bPlusTree.showTree();
+bPlusTree.stringShowTree();
 
 cout << endl << endl << endl;
 
-bPlusTree.listValues();
+bPlusTree.stringListValues();
 
 getch();
 
